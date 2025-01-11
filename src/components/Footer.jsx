d0083945d87b0fa0f019625678c94a6b227fdf95
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2025-01-11 02:05:46 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+
 
 export default function Footer() {
     
 
     return (
-        <>
-            <section className=' bg-gray-700 text-white py-20'>
-                <p className='text-3lx text-center sm:text-2lx'>�� 2022 IMA. All rights reserved.</p>
+        <> 
+            <section className='bg-gray-800 text-white py-40'>
+                <p className='text-3lx text-center sm:text-2lx'> &copy; 2025 IMA. All rights reserved.</p>
             </section>
         </>
     )
